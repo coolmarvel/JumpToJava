612d84b0d12ae555c3c6f81911bc4b4e1993e8b4
이성현 <marvel97@naver.com>
2022-06-29 15:48:54 +0900

    예외처리 종합문제

---
+++ TotalTest/ExceptionTest.java
@@ -0,0 +1,31 @@
+package TotalTest;
+
+import java.util.Random;
+
+class OddException extends Exception {
+    public void error() {
+        System.out.println("error");
+    }
+}
+
+public class ExceptionTest {
+    static void execute(int n) throws OddException {
+        System.out.printf("입력 숫자: %d\n", n);
+        if (n % 2 == 1) { // 홀수이면 OddException이 발생한다.
+            throw new OddException();
+        }
+        System.out.println("짝수입니다.");
+    }
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            try {
+                execute(r.nextInt(10));
+            } catch (OddException e) {
+                // e.printStackTrace();
+                e.error();
+            }
+        }
+    }
+}
