38d94de672f487565175094bb4004f3dd95dd7ef
이성현 <marvel97@naver.com>
2022-06-29 14:24:14 +0900

    게시판 페이징하기

--- JAVAPROGRAMMING/Page.java
+++ JAVAPROGRAMMING/Page.java
@@ -2,11 +2,16 @@ package JAVAPROGRAMMING;
 
 public class Page {
     static int getTotalPage(int m, int n) {
-        return m / n + 1;
+        if (m % n == 0) {
+            return m / n;
+        } else {
+            return m / n + 1;
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(getTotalPage(5, 10));
+        System.out.println(getTotalPage(10, 10));
         System.out.println(getTotalPage(15, 10));
         System.out.println(getTotalPage(25, 10));
         System.out.println(getTotalPage(30, 10));
