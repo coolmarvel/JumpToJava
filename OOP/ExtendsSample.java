24c68ec6aa844ee6b86310c8b90cde0da7c6615f
이성현 <marvel97@naver.com>
2022-06-29 09:18:40 +0900

    All 주석

--- OOP/ExtendsSample.java
+++ OOP/ExtendsSample.java
@@ -1,34 +1,34 @@
-package OOP;
+// package OOP;
 
-class Animal {
-    String name;
+// class Animal {
+//     String name;
 
-    void setName(String name) {
-        this.name = name;
-    }
-}
+//     void setName(String name) {
+//         this.name = name;
+//     }
+// }
 
-class Dog extends Animal {
-    void sleep() {
-        System.out.println(this.name + " zzz");
-    }
-}
+// class Dog extends Animal {
+//     void sleep() {
+//         System.out.println(this.name + " zzz");
+//     }
+// }
 
-class HouseDog extends Dog {
-    void sleep() {
-        System.out.println(this.name + " zzz in house");
-    }
+// class HouseDog extends Dog {
+//     void sleep() {
+//         System.out.println(this.name + " zzz in house");
+//     }
 
-    void sleep(int hour) {
-        System.out.println(this.name + " zzz in house for " + hour + " hours");
-    }
-}
+//     void sleep(int hour) {
+//         System.out.println(this.name + " zzz in house for " + hour + " hours");
+//     }
+// }
 
-public class ExtendsSample {
-    public static void main(String[] args) {
-        HouseDog houseDog = new HouseDog();
-        houseDog.setName("happy");
-        houseDog.sleep(); // happy zzz 출력
-        houseDog.sleep(3);  // happy zzz in house for 3 hours 출력
-    }
-}
+// public class ExtendsSample {
+//     public static void main(String[] args) {
+//         HouseDog houseDog = new HouseDog();
+//         houseDog.setName("happy");
+//         houseDog.sleep(); // happy zzz 출력
+//         houseDog.sleep(3);  // happy zzz in house for 3 hours 출력
+//     }
+// }
--- OOP/InterfaceSample.java
+++ OOP/InterfaceSample.java
@@ -1,41 +1,41 @@
-package OOP;
+// package OOP;
 
-interface Predator {
-    String getFood();
-}
+// interface Predator {
+//     String getFood();
+// }
 
-class Animal {
-    String name;
+// class Animal {
+//     String name;
 
-    void setName(String name) {
-        this.name = name;
-    }
-}
+//     void setName(String name) {
+//         this.name = name;
+//     }
+// }
 
-class Tiger extends Animal implements Predator {
-    public String getFood() {
-        return "apple";
-    }
-}
+// class Tiger extends Animal implements Predator {
+//     public String getFood() {
+//         return "apple";
+//     }
+// }
 
-class Lion extends Animal implements Predator {
-    public String getFood() {
-        return "banana";
-    }
-}
+// class Lion extends Animal implements Predator {
+//     public String getFood() {
+//         return "banana";
+//     }
+// }
 
-class ZooKeeper {
-    void feed(Predator predator) {
-        System.out.println("feed " + predator.getFood());
-    }
-}
+// class ZooKeeper {
+//     void feed(Predator predator) {
+//         System.out.println("feed " + predator.getFood());
+//     }
+// }
 
-public class InterfaceSample {
-    public static void main(String[] args) {
-        ZooKeeper zooKeeper = new ZooKeeper();
-        Tiger tiger = new Tiger();
-        Lion lion = new Lion();
-        zooKeeper.feed(tiger); // feed apple 출력
-        zooKeeper.feed(lion); // feed banana 출력
-    }
-}
+// public class InterfaceSample {
+//     public static void main(String[] args) {
+//         ZooKeeper zooKeeper = new ZooKeeper();
+//         Tiger tiger = new Tiger();
+//         Lion lion = new Lion();
+//         zooKeeper.feed(tiger); // feed apple 출력
+//         zooKeeper.feed(lion); // feed banana 출력
+//     }
+// }
