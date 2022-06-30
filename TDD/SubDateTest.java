55c41fe0f09f682ed4cafacfed39b7c11c4a3ef8
이성현 <marvel97@naver.com>
2022-06-30 10:32:53 +0900

    junit test 해보기

---
+++ TDD/SubDateTest.java
@@ -0,0 +1,11 @@
+package TDD;
+
+import junit.framework.TestCase;
+
+public class SubDateTest extends TestCase {
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(SubDateTest.class);
+    }
+
+}
